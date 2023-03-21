e628b6343d474de96ff40e92bc3a39af22c2d765
Sree2604 <ds04aranganthan@gmail.com>
2023-03-21 19:02:53 +0530

    Just for exercise....

---
+++ test.py
@@ -0,0 +1,42 @@
+items = {
+    'coffee' : 
+    {
+        'name' : 'coffee',
+        'price' : 20,
+        'stock' : 20,
+        'refill' : 20,
+        'profit' : 8,
+        'sales' : 0
+    },
+    'tea' : 
+    {
+        'name' : 'tea',
+        'price' : 25,
+        'stock' : 35,
+        'refill' : 35,
+        'profit' : 10,
+        'sales' : 0
+    },
+    'sandwich' : 
+    {
+        'name' : 'sandwich',
+        'price' : 45,
+        'stock' : 25,
+        'refill' : 25,
+        'profit' : 18,
+        'sales' : 0
+    },
+    'fries' : 
+    {
+        'name' : 'fries',
+        'price' : 30,
+        'stock' : 30,
+        'refill' : 30,
+        'profit' : 5,
+        'sales' : 0
+    }
+}
+
+
+for i,j in items.items():
+    print(i)
