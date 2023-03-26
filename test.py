856576b0f92b28438e534e62e6baac436c53972e
Sree2604 <ds04aranganthan@gmail.com>
2023-03-26 11:12:32 +0530

    Just for exercise....

--- test.py
+++ test.py
@@ -1,42 +1,38 @@
-items = {
-    'coffee' : 
-    {
-        'name' : 'coffee',
-        'price' : 20,
-        'stock' : 20,
-        'refill' : 20,
-        'profit' : 8,
-        'totalProfit' : 250,
-        'sales' : 56
-    },
-    'tea' : 
-    {
-        'name' : 'tea',
-        'price' : 25,
-        'stock' : 35,
-        'refill' : 35,
-        'profit' : 10,
-        'totalProfit' : 400,
-        'sales' : 54
-    },
-    'sandwich' : 
-    {
-        'name' : 'sandwich',
-        'price' : 45,
-        'stock' : 25,
-        'refill' : 25,
-        'profit' : 18,
-        'totalProfit' : 310,
-        'sales' : 92
-    },
-    'fries' : 
-    {
-        'name' : 'fries',
-        'price' : 30,
-        'stock' : 30,
-        'refill' : 30,
-        'profit' : 5,
-        'totalProfit' : 280,
-        'sales' : 76
-    }
-}
+Input = 'sabcyursdfsb'
+
+def findC(Input):
+    index1 = Input.find('c')
+    index2 = Input.rfind('c')
+    if index1 != -1 and index2 != -1:
+        if index1 == index2:
+            print("There is only one 'c' in the string...")
+        else:
+            print(Input[index1+1:index2])
+    else:
+        print("There are no letters a,b and c in the given input")
+
+def findB(Input):
+    index1 = Input.find('b')
+    index2 = Input.rfind('b')
+    if index1 != -1 and index2 != -1:
+        if index1 == index2:
+            print("There is only one 'b' in the string...")
+            findC(Input)
+        else:
+            print(Input[index1+1:index2])
+    else:
+        findC(Input)
+
+def findA(Input):
+    index1 = Input.find('a')
+    index2 = Input.rfind('a')
+    if index1 != -1 and index2 != -1:
+        if index1 == index2:
+            print("There is only one 'a' in the string...")
+            findB(Input)
+        else:
+            print(Input[index1+1:index2])
+    else:
+        findB(Input)
+
+findA(Input)
