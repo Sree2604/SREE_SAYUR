76fdbc76fd8983868d951103a7721e74a68fcd24
Sree2604 <ds04aranganthan@gmail.com>
2023-04-09 19:55:03 +0530

    Completed....

--- List/commonInArrays.py
+++ List/commonInArrays.py
@@ -1,8 +1,14 @@
+'''
+THere are two arrays of numbers. the numbers are sorted in ascending order. 
+Find the numbers that are common in both arrays. 
+Eg - array 1 = [1,3,7,9,13,14], array2 [1,2,7,13,15]. answer - [1,7,13]
+'''
 array1 = [1,3,7,9,13,14]
 array2 = [1,2,7,13,15]
+
 largeArray = []
 smallArray = []
-result = []
+
 if len(array1) > len(array2):
     largeArray = array1
     smallArray = array2
@@ -10,9 +16,7 @@ else:
     largeArray = array2
     smallArray = array1
 
+print("The common values in both the array is...")
 for i in largeArray:
     if i in smallArray:
-        result.append(i)
-
-print("The common values in both the array is...")
-print(result)
+        print(f'{i}')
