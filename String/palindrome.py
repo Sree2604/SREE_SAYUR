c772e30f41bc24e8cc04cae16a0d1b2d64bea250
Sree2604 <ds04aranganthan@gmail.com>
2023-04-28 21:52:15 +0530

    Comments want to be write...

---
+++ String/palindrome.py
@@ -0,0 +1,31 @@
+""" def isPalindrome(string):
+
+    if len(string) < 2:
+        return True
+    elif string[0] == string[-1]:
+        return isPalindrome(string[1:-1])
+    else:
+        return False
+
+inputString = input("Enter a string: ")
+if isPalindrome(inputString):
+    print("It is a palindrome...")
+else:
+    print("It is not a palindrome...") """
+
+def checkPalindrome(string):
+    start = 0
+    end = 1
+    while(start<end):
+           if(string[start] == string[end]):
+               start = start + 1
+               end = -(end + 1)
+           else:
+               return False
+    return True
+
+inputString = input("Enter the string: ")
+if checkPalindrome(inputString):
+    print("It is a palindrome...")
+else:
+    print("It is not a palindrome...")
