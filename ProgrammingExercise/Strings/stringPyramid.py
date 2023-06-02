8bb20056af6b595a60eb3c6baaa6667a88116c27
Sree2604 <ds04aranganthan@gmail.com>
2023-06-03 00:34:44 +0530

    Completed...

---
+++ ProgrammingExercise/Strings/stringPyramid.py
@@ -0,0 +1,19 @@
+# ########## Program 2
+# #Print your name in a pyramid
+# #eg RAM
+# #R
+# #RA
+# #RAM
+
+myName = 'RAM'
+outputString = ''
+for letter in myName:
+    outputString += letter
+    print(outputString)
+
+"""
+Output:
+R
+RA
+RAM
+"""
